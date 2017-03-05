08d5cc481672e1b9c966f4788c9b7eff2955914f
intellectape <adi_b09@hotmail.com>
2017-03-05 18:34:56 -0500

    Adding updated files.

--- client.py
+++ client.py
@@ -1,28 +1,74 @@
 import sys
+import os
 import socket
+import time
 
-# Using slow start approach for getting faster results
 
 if len(sys.argv) != 3:
     print("python client.py <server address> <server port>")
     exit(1)
 
-initialPacket = 50 * 1024
-sendingPacket = 5 * 1024 * 1024
+initialPacket = 10 * 1024
+sendingPacket = 1 * 1024 * 1024
+handshake = 20
+ACK = 41
 
-s = socket.socket() 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = sys.argv[1]
 port = int(sys.argv[2])
 s.connect((host, port))
 
-print(s.recv(1024))
-s.close
+def packetCreation(packetsize):
+    return bytearray(os.urandom(packetsize))
+
+def upload():
+    global ACK, s
+    try:
+        packet = packetCreation(sendingPacket)
+        
+        start_time = int(round(time.time() * 1000))
+        s.send(packet)
+        flag = s.recv(ACK)
+
+        if not flag:
+            print("No acknowledgment for upload received.")
+            exit(0)
+
+        end_time = int(round(time.time() * 1000))
+
+        rtt = end_time -start_time
+        up_speed = (len(packet) * 1000 * 8) / rtt
+        return up_speed
+    except:
+        print("An Exception has occurred in the code.")
+        return 0
+
+def download():
+    global s, sendingPacket
+    try:
+        s.send(bytes(handshake))
+
+        start_time = int(round(time.time() * 1000))
+
+        receive_packet = s.recv(sendingPacket)
+
+        end_time = int(round(time.time() * 1000))
+
+        rtt = end_time - start_time
+        down_speed = (len(receive_packet) * 1000 * 8) / rtt
+        return down_speed
+    except:
+        print("An Exception has occurred in the code.")
+        return 0
 
 def main():
-    global initialPacket, sendingPacket
-    
+    global initialPacket, sendingPacket, s
+    down_speed = download()
+    up_speed = upload()
 
-    return True
+    print("Download Speed: " + str(down_speed/ (1024 * 1024)) + " Mbits/sec")
+    print("Upload Speed: " + str(up_speed / (1024 * 1024)) + " Mbits/sec")
+    s.close
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,14 +1,65 @@
 import sys
+import os
 import socket
+
 
 if len(sys.argv) != 3:
     print("python client.py <server address> <server port>")
     exit(1)
 
+initialPacket = 10 * 1024
+sendingPacket = 1 * 1024 * 1024
+handshake = 20
+ACK = 41
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind((host, port))
+
+def packetCreation(packetsize):
+    return bytearray(os.urandom(packetsize))
+
 def main():
-    while True:
+    global initialPacket, sendingPacket, s
+    s.listen(5)
+    print("Server Up!")
+    c, address = s.accept()
+    
+    while True:    
+        print("Connected to :", address)
+        packet = packetCreation(sendingPacket)
+        test_status = clientDownload(c, packet)
         
+        if not test_status:
+            print("Executiong Failed: Client Download Test")
+        
+        clientUpload(c)
+        print("Test Completed!")
+        print("#####################################")
+        s.close()
+
+def clientUpload(client):
+    global sendingPacket
+
+    data = client.recv(sendingPacket)
+
+    if not data:
+        print("Execution Failed: Client Upload Test ")
+        exit(0)
+    client.send(bytes(ACK))
+    print("Upload Acknowledgment sent.")
+
+def clientDownload(client, packet):
+
+    hands = client.recv(handshake)
+    if not hands:
+        print("No Handshake from Client.")
+    
+    client.send(packet)
+    print("Packet sent to Client.")
     return True
+
 
 if __name__ == "__main__":
     main()
