658b1fe4a2d8a60ef3647f5a76510569c7195638
intellectape <adi_b09@hotmail.com>
2017-03-05 18:59:28 -0500

    Adding final updated files.

--- client.py
+++ client.py
@@ -8,19 +8,34 @@ if len(sys.argv) != 3:
     print("python client.py <server address> <server port>")
     exit(1)
 
-initialPacket = 10 * 1024
-sendingPacket = 1 * 1024 * 1024
+# Variables for data packet size and handshake, ACK for establishing communication over the network
+initialPacket = 1024
+sendingPacket = 10 * 1024
 handshake = 20
 ACK = 41
 
+# Initializing the socket for the Client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 host = sys.argv[1]
 port = int(sys.argv[2])
 s.connect((host, port))
 
+# Function to eliminate slow start problem of TCP
+def slowStartEnd():
+    global s
+    try:
+        packet = packetCreation(initialPacket)
+        s.send(packet)
+        data = s.recv(initialPacket)
+    except:
+        print("Exception have occurred in the code!")
+        exit(0)
+
+# Function to create Packets at random
 def packetCreation(packetsize):
     return bytearray(os.urandom(packetsize))
 
+# Function to test Upload Functionality fo the client
 def upload():
     global ACK, s
     try:
@@ -36,17 +51,24 @@ def upload():
 
         end_time = int(round(time.time() * 1000))
 
+        # rtt is the trip time from one side
         rtt = end_time -start_time
+        
+        # if the connection is on local network, then to avoid error in upload time
+        if rtt == 0:
+            rtt = 1
         up_speed = (len(packet) * 1000 * 8) / rtt
         return up_speed
     except:
         print("An Exception has occurred in the code.")
         return 0
 
+# Function to test Download Functionality of the Client
 def download():
     global s, sendingPacket
     try:
         s.send(bytes(handshake))
+        print("Sending handshake to the server signalling readiness for receiving data.")
 
         start_time = int(round(time.time() * 1000))
 
@@ -54,7 +76,14 @@ def download():
 
         end_time = int(round(time.time() * 1000))
 
+        print("Received packet.")
+
+        # rtt is the trip time from one side
         rtt = end_time - start_time
+
+        # if the connection is on local network, then to avoid error in download time
+        if rtt == 0:
+            rtt = 1
         down_speed = (len(receive_packet) * 1000 * 8) / rtt
         return down_speed
     except:
@@ -63,6 +92,8 @@ def download():
 
 def main():
     global initialPacket, sendingPacket, s
+    
+    slowStartEnd()
     down_speed = download()
     up_speed = upload()
 
--- server.py
+++ server.py
@@ -7,37 +7,34 @@ if len(sys.argv) != 3:
     print("python client.py <server address> <server port>")
     exit(1)
 
-initialPacket = 10 * 1024
-sendingPacket = 1 * 1024 * 1024
+initialPacket = 1024
+sendingPacket = 10 * 1024 
 handshake = 20
 ACK = 41
 
+# Initializing the socket for the Server
 host = sys.argv[1]
 port = int(sys.argv[2])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.bind((host, port))
+s.listen(5)
+print("Server Up!")
+c, address = s.accept()
 
+# Function to eliminate slow start problem of TCP
+def slowStartEnd():
+    global c
+    try:
+        data = c.recv(initialPacket)
+        packet = packetCreation(initialPacket)
+        c.send(packet)
+    except:
+        print("Exception have occurred in the code!")
+        exit(0)
+
+# Function to create Packets at random
 def packetCreation(packetsize):
     return bytearray(os.urandom(packetsize))
-
-def main():
-    global initialPacket, sendingPacket, s
-    s.listen(5)
-    print("Server Up!")
-    c, address = s.accept()
-    
-    while True:    
-        print("Connected to :", address)
-        packet = packetCreation(sendingPacket)
-        test_status = clientDownload(c, packet)
-        
-        if not test_status:
-            print("Executiong Failed: Client Download Test")
-        
-        clientUpload(c)
-        print("Test Completed!")
-        print("#####################################")
-        s.close()
 
 def clientUpload(client):
     global sendingPacket
@@ -60,6 +57,22 @@ def clientDownload(client, packet):
     print("Packet sent to Client.")
     return True
 
+def main():
+    global initialPacket, sendingPacket, s, c, address
+    slowStartEnd()
+    
+    while True:    
+        print("Connected to :", address)
+        packet = packetCreation(sendingPacket)
+        test_status = clientDownload(c, packet)
+        
+        if not test_status:
+            print("Execution Failed: Client Download Test")
+        
+        clientUpload(c)
+        print("Test Completed!")
+        print("#####################################")
+        s.close()
 
 if __name__ == "__main__":
     main()
